fccd9190db31373b1df61c4c153b8ae5aed765b8
RiseGhost <jomiguelsa2mt@gmail.com>
2022-03-26 02:14:29 +0000

    Exercício 3 pronto

--- Lab03/pauta-online.php
+++ Lab03/pauta-online.php
@@ -2,14 +2,19 @@
 
 $xml= simplexml_load_file('pauta.xml');
 
-
-$dom = new DOMDocument('1.0', 'utf-8');
-
+$NotaFinal = 0;
 for ($i = 0; $i <= 4; $i++){
     echo 'Número: ' .$xml->cd[$i]->number."\n";
     echo 'Nome: ' .$xml->cd[$i]->name."\n";
     echo 'Nota Frequência 1: ' .$xml->cd[$i]->Exam1."\n";
     echo 'Nota Frequência 2: ' .$xml->cd[$i]->Exam2."\n";
+    $NotaFinal = ($xml->cd[$i]->Exam1 + $xml->cd[$i]->Exam2)/2;
+    echo 'Nota Final: ' .$NotaFinal."\n";
+    if($NotaFinal >= 10){
+        echo "Aluno aprovado.\n";
+    }   else{
+        echo "Aluno reprovado.\n";
+    }
     echo "\n";
 }
 ?>
