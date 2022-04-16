e568681433526ce1f54e29034bf5d24d8bfaddc1
RiseGhost <jomiguelsa2mt@gmail.com>
2022-04-16 16:03:08 +0100

    Finished

---
+++ Assessemt Lab 03/Exercício 4/Readme.md
@@ -0,0 +1,8 @@
+# O que podes encontrar aqui: ❓
+O objetivo deste mini projeto é permitir ao utilizador insirir dados sobre pessoas e depois fazer o calculo de quantas pessoas foram introduzidas, da idade minina, idade máxima e média de idades. Todos esse calculos são efetuados graças ao ficheiro __scrpti.js__. <p>
+Para interrogar o utilizadar foi utilizada a função __promt__ do __JavaScript__.
+
+### Algumas imagens do site: 🖼️
+![1](https://user-images.githubusercontent.com/91985039/163679988-58fa29a0-3cb6-40eb-9787-c70d657a0849.jpg)
+![2](https://user-images.githubusercontent.com/91985039/163679997-af08473a-ad9a-4f94-a9e4-a5707b142675.jpg)
+![3](https://user-images.githubusercontent.com/91985039/163679998-426497e1-5f87-462c-834b-60e9ef33f85b.jpg)
--- Assessemt Lab 03/Exercício 4/scrpti.js
+++ Assessemt Lab 03/Exercício 4/scrpti.js
@@ -11,7 +11,7 @@ function registo(){
         array.push(user);
     }
     
-    
+    //Calculo da maior idade:
     var maior = -1;
     for(let i = 0; i < array.length; i++){
         if(array[i].age > maior){
@@ -19,7 +19,7 @@ function registo(){
         }
     }
     
-    
+    //Calculo da menor idade:
     let menor = 999999;
     for(let i = 0; i < array.length; i++){
         if(array[i].age < menor){
@@ -27,7 +27,7 @@ function registo(){
         }
     }
 
-
+    //Calculo da media das idades:
     let soma = 0;
     for(let i = 0; i < array.length; i++){
         soma += array[i].age;
@@ -36,7 +36,7 @@ function registo(){
     console.log("array.length: " + array.length);
     let average = soma / array.length;
 
-
+    //Colocação dos dados na labels:
     document.getElementById("Maximum").innerHTML = document.getElementById("Maximum").innerHTML + maior;
     document.getElementById("Minimum").innerHTML = document.getElementById("Minimum").innerHTML + menor;
     document.getElementById("Average").innerHTML = document.getElementById("Average").innerHTML + average;
