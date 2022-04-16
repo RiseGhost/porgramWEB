d1291e7dcacc1cba49501ea986e1b4f4ee17cf19
RiseGhost <jomiguelsa2mt@gmail.com>
2022-04-16 15:22:49 +0100

    game

---
+++ Assessemt Lab 03/Exercício 5/Readme.md
@@ -0,0 +1,13 @@
+# O que podes encontrar aqui: ❓
+O objetivo deste mini projeto foi a criação de um mini jogo 🕹️ utilizando __JavaScript__ e __HTML__. Não foi utilizada a tag __canvas__ do HTMl para fazer aparecer as formas. Em vez disso foi utilizada uma div normal. <p>
+Consegui fazer a div se mexer pelo cenário manipulando as suas margins. Para trocar a forma dela foi utilizada a tag __border-radius__ e para trocar a cor foi simplesmente trocar a __background-color__ da div. <p>
+Todas essa manipulações da div foram efetuadas no ficheiro __game.js__.
+
+### Como funciona o jogo?: 🎮
+Paracerem no cenário a forma de um quadrado ou circulo, em um lugar aleatório e o objetivo do jogador é clicar lá o mais rápido possivel. O melhor tempo é registado e o tempo atual também é registado. <p>
+Caso o jogador falhe o clicke na forma, é gerada um nova forma e o tempo é resetado para 0.
+
+### Algumas imagens do site: 🖼️
+![1](https://user-images.githubusercontent.com/91985039/163678602-451b94a7-3338-4da6-b6e1-ffd2e12254cc.jpg)
+![2](https://user-images.githubusercontent.com/91985039/163678604-b45c9f17-a291-43e6-95bd-d303c9382b78.jpg)
+![3](https://user-images.githubusercontent.com/91985039/163678606-c7be6bea-67bd-4b09-86a0-b85b6daecffd.jpg)
---
+++ Assessemt Lab 03/Exercício 5/game.js
@@ -0,0 +1,43 @@
+var clickedTime = 0;
+var reactionTime = 999999999;
+var createdTime = 0;
+
+function randomShape() {
+    var randomNumber = Math.floor(Math.random() * 2);
+    switch (randomNumber) {
+        case 0:
+            return "square";
+        case 1:
+            return "circle";
+    }
+}
+
+function randomColor() {
+    return randomNumber = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+}
+
+function makebox() {
+    var shape = randomShape();
+    var game = document.getElementById("divbox");
+    game.style.display = "block";
+    if(shape == "square") {
+        game.style.borderRadius = "0px";
+    }   else {
+        game.style.borderRadius = "100%";
+    }
+    game.style.marginLeft = Math.floor(Math.random() * 600) + "px";
+    game.style.marginTop = Math.floor(Math.random() * 300) + "px";
+    game.style.backgroundColor = randomColor();
+    createdTime = Date.now();
+}
+
+function validate(){
+    clickedTime = Date.now();
+    var timeDifference = (clickedTime - createdTime) / 1000;
+    if(timeDifference < reactionTime) {
+        reactionTime = timeDifference;
+    }
+    document.getElementById("reactionTime").innerHTML = timeDifference;
+    document.getElementById("recordTime").innerHTML = reactionTime;
+    clickedTime = 0;
+}
