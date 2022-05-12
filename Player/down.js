3d84c00d276acd7518232c87ed64b0c2c7eef834
RiseGhost <jomiguelsa2mt@gmail.com>
2022-05-12 11:53:05 +0100

    update

---
+++ Player/down.js
@@ -0,0 +1,14 @@
+var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+
+//Configure YoutubeMp3Downloader with your settings
+var YD = new YoutubeMp3Downloader({
+    "ffmpegPath": "C:\\Users\\jomig\\FFmpeg\\bin\\ffmpeg.exe",        // FFmpeg binary location
+    "outputPath": "C:/Users/jomig",         // Output file location (default: the home directory)
+    "youtubeVideoQuality": "highestaudio",  // Desired video quality (default: highestaudio)
+    "queueParallelism": 2,                  // Download parallelism (default: 1)
+    "progressTimeout": 2000,                // Interval in ms for the progress reports (default: 1000)
+    "allowWebm": false                      // Enable download from WebM sources (default: false)
+});
+
+//Download video and save as MP3 file
+YD.download("gdZLi9oWNZg");
