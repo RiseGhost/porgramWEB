29aa37a830ec9b7d68399c74c58b5998ed01cfd4
RiseGhost <91985039+RiseGhost@users.noreply.github.com>
2022-06-28 21:05:12 +0100

    Delete UBi Galaxy Music directory

--- UBi Galaxy Music/public/Script/Validate.js
+++
@@ -1,9 +0,0 @@
-function validate() {
-    var password = document.getElementById('password').value;
-    var confirm_password = document.getElementById('confirm-password').value;
-    if (password != confirm_password) {
-        alert("Password not match");
-    } else {
-        document.getElementById('formualrio').submit();
-    }
-}
